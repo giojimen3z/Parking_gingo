4d683924a6fd6f7de34b05078d9e0a6fc765eaf8
jaime--jimenez <jaime.jimenez@ppm.com.ec>
2024-06-16 21:42:03 -0500

    adding cors and docker file

--- main.go
+++ main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"time"
+
 	"Paking_gingo/cmd/api/controllers"
 	"Paking_gingo/cmd/api/database"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 	database.ConnectDatabase()
+
+	// Configurar CORS
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	r.GET("/vehicles", controllers.FindVehicles)
 	r.GET("/vehicles/:license_plate", controllers.FindVehicleByLicensePlate)
